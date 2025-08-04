9dcb5a24baad3bc1310b7daa11dc56f30d06c19c
johantchulacan <johant.chulacan@campusucc.edu.co>
2025-08-04 14:06:08 +0000

    clase 1

---
+++ clase 1/Clase1.java
@@ -0,0 +1,22 @@
+public class Clase1 {
+    public static void main(String[] args) {
+        System.out.println("estructura de datos");
+
+        //arreglos o arrays o vectores
+
+        //declaracion del arreglo. forma 1
+        //int[] numeros;
+        //numeros = new int [5];
+
+        //declaracion del arreglo .forma 2
+        //int [] numeros =new int [5]
+
+        //Declaración e inicialización
+        int[] numeros = {10, 20, 30, 40, 50};
+
+
+
+
+
+    }
+}
