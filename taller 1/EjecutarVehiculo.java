cf0f8dd8289463e33135b5fccd86dfb6684503cd
johantchulacan <johant.chulacan@campusucc.edu.co>
2025-08-27 01:51:29 +0000

    formato al documento

--- taller 1/EjecutarVehiculo.java
+++ taller 1/EjecutarVehiculo.java
@@ -1,19 +1,17 @@
 import java.util.*;
 
-
 public class EjecutarVehiculo {
-    public static void main (String[] args) throws Exception {
-        
+        public static void main(String[] args) throws Exception {
 
                 Vehiculo[] veh = new Vehiculo[5];
-                        
-                                veh[0] = new Vehiculo("BMW", 2000, "WSE2324343DS", 2000, true);
-                                        veh[1] = new Vehiculo("KIA", 2025, "OP9864HYHHYW", 2025, false);
-                                                veh[2] = new Vehiculo("CHEVROLET", 2005, "234VBGT4334F", 2005, true);
-                                                        veh[3] = new Vehiculo("PEGOUT", 2010, "QAWE3ER4RT66", 2010, false);
-                                                                veh[4] = new Vehiculo("MERCEDEZ BENZ", 1970, "23SDER5443DF", 1970, true);
-                                                                        
-                                                                                System.out.println(veh[0].iterarVehiculo(veh));
 
-                                                                                    }
-                                                                                    }
+                veh[0] = new Vehiculo("BMW", 2000, "WSE2324343DS", 2000, true);
+                veh[1] = new Vehiculo("KIA", 2025, "OP9864HYHHYW", 2025, false);
+                veh[2] = new Vehiculo("CHEVROLET", 2005, "234VBGT4334F", 2005, true);
+                veh[3] = new Vehiculo("PEGOUT", 2010, "QAWE3ER4RT66", 2010, false);
+                veh[4] = new Vehiculo("MERCEDEZ BENZ", 1970, "23SDER5443DF", 1970, true);
+
+                System.out.println(veh[0].iterarVehiculo(veh));
+
+        }
+}
